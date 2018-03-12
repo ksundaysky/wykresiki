d69cb6bf6afe48359c863d8ad18b6c1c3ddc8ef5
Krzysztof Niedzielski <krzysztof.niedzielski96@gmail.com>
2018-03-12 18:02:13 +0100

    wykresiki i buttony cd 1.0.1

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -1,13 +1,19 @@
 package sample;
 
 import com.emxsys.chart.extension.LogarithmicAxis;
+import javafx.animation.KeyFrame;
+import javafx.animation.Timeline;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.geometry.Pos;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.SnapshotParameters;
 import javafx.scene.control.Button;
 import javafx.scene.effect.Reflection;
+import javafx.scene.image.WritableImage;
+import javafx.scene.input.Clipboard;
+import javafx.scene.input.ClipboardContent;
 import javafx.scene.layout.Priority;
 import javafx.scene.layout.StackPane;
 import javafx.scene.layout.VBox;
@@ -25,21 +31,33 @@ import java.util.ArrayList;
 import java.util.List;
 
 import javafx.scene.chart.ValueAxis;
+import javafx.util.Duration;
 
 import static java.lang.Math.pow;
 
 
 public class Main extends Application {
 
-    @Override public void start(Stage stage) {
+    public List<Float> X;
+    public List<Float> Y;
+    public List<Float> Parameters;
+
+    public static int s = 50;
+
+    public ArrayList<XYChart.Series> array;
+
+
+
+    @Override
+    public void start(Stage stage) {
 
 
 
         //String fileName = "BAZA2.bin";
         String fileName = "BAZA22.bin";
-        List<Float> X = new ArrayList<>();
-        List<Float> Parameters = new ArrayList<>();
-        List<Float> Y = new ArrayList<>();
+        this.X = new ArrayList<>();
+        this.Parameters = new ArrayList<>();
+        this.Y= new ArrayList<>();
 
         try {
             FileInputStream fileIs = new FileInputStream(fileName);
@@ -79,13 +97,14 @@ public class Main extends Application {
 
 
         LineChart<Double,Double> chart = new LineChart(xAxis,yAxis);
+        chart.setAnimated(false);
 
-        XYChart.Series series = new XYChart.Series();
+        XYChart.Series<Double,Double> series = new XYChart.Series();
         series.setName("Portfolio 1");
         XYChart.Series series2 = new XYChart.Series();
         series2.setName("Portfolio 1");
 
-        ArrayList<XYChart.Series> array = new ArrayList<>();
+        array = new ArrayList<>();
 
         for (int i =0; i<38;i++)
         {
@@ -100,6 +119,9 @@ public class Main extends Application {
             {
                 int temp = i*120+j;
                 array.get(i).getData().add(new XYChart.Data(pow(10,X.get(j)),pow(10,Y.get(temp))));
+                //XYChart.Data<Double,Double> num = array.get(i).getData();
+
+
             }
         }
 
@@ -107,19 +129,23 @@ public class Main extends Application {
         spLineChart.getChildren().add(chart);
 
         Button button = new Button("Up");
+        Button button2 = new Button("Down");
         button.setOnMouseClicked((event)->{
-            System.out.println("You just clicked me");
+
+            s++;
+            update(button,button2);
         });
 
         button.setTranslateX(50);
         //button.setTranslateX(10);
 
 
+        System.out.println(Y.size());
 
-
-        Button button2 = new Button("Down");
         button2.setOnMouseClicked((event)->{
-            System.out.println("You just clicked me");
+
+            s--;
+            update(button,button2);
         });
 
         StackPane spButton = new StackPane();
@@ -139,11 +165,69 @@ public class Main extends Application {
         chart.setCreateSymbols(false);
 
 
+
+
+        chart.applyCss();
+        chart.layout();
         stage.setScene(scene);
         stage.show();
+
+
+            WritableImage image = chart.snapshot(new SnapshotParameters(),null);
+           // WritableImage image = new WritableImage(600,800);
+
+            chart.snapshot(new SnapshotParameters(),image);
+            ClipboardContent cc = new ClipboardContent();
+            cc.putImage(image);
+            Clipboard.getSystemClipboard().setContent(cc);
+
+
+//        Timeline timeline = new Timeline(new KeyFrame(Duration.millis(1000), e->run()));
+//        timeline.setCycleCount(timeline.INDEFINITE);
+//        timeline.play();
     }
 
 
+        public void run()
+        {
+
+        }
+        public void update(Button b1,Button b2)
+        {
+            System.out.println(s);
+
+            if(s>=607)
+            {
+                b1.setDisable(true);
+                b2.setDisable(false);
+            }
+            if(s<=0)
+            {
+                b2.setDisable(true);
+                b1.setDisable(false);
+            }
+
+            if(s>0 && s <607)
+            {
+                b2.setDisable(false);
+                b1.setDisable(false);
+            }
+
+
+            for(int i =0; i<38;i++)
+            {
+                array.get(i).getData().clear();
+                for(int j = 0; j < 120;j++)
+                {
+                   // int temp = i*s*120+j;
+
+                    int temp = s*(120*38)+(i*120)+j;
+
+                    array.get(i).getData().add(new XYChart.Data(pow(10,X.get(j)),pow(10,Y.get(temp))));
+
+                }
+            }
+        }
     public static void main(String[] args) {
         launch(args);
     }

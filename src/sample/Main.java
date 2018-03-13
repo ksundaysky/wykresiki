e86b3f38bb3cdf91bc045173ba91107a412d4b46
Krzysztof Niedzielski <krzysztof.niedzielski96@gmail.com>
2018-03-13 11:41:06 +0100

    Dziala wszystko oprocz D/d?

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -4,16 +4,21 @@ import com.emxsys.chart.extension.LogarithmicAxis;
 import javafx.animation.KeyFrame;
 import javafx.animation.Timeline;
 import javafx.application.Application;
+import javafx.beans.property.SimpleStringProperty;
+import javafx.beans.property.StringProperty;
+import javafx.event.EventHandler;
 import javafx.fxml.FXMLLoader;
 import javafx.geometry.Pos;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.SnapshotParameters;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.Menu;
+import javafx.scene.control.MenuItem;
 import javafx.scene.effect.Reflection;
 import javafx.scene.image.WritableImage;
-import javafx.scene.input.Clipboard;
-import javafx.scene.input.ClipboardContent;
+import javafx.scene.input.*;
 import javafx.scene.layout.Priority;
 import javafx.scene.layout.StackPane;
 import javafx.scene.layout.VBox;
@@ -29,9 +34,11 @@ import javafx.stage.Stage;
 import java.io.*;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Properties;
 
 import javafx.scene.chart.ValueAxis;
 import javafx.util.Duration;
+import sun.tools.jar.CommandLine;
 
 import static java.lang.Math.pow;
 
@@ -42,9 +49,13 @@ public class Main extends Application {
     public List<Float> Y;
     public List<Float> Parameters;
 
-    public static int s = 50;
+    public static int s = 100;
 
     public ArrayList<XYChart.Series> array;
+
+    public LineChart<Double,Double> chart;
+    public Scene scene;
+    public Label label;
 
 
 
@@ -53,8 +64,8 @@ public class Main extends Application {
 
 
 
-        //String fileName = "BAZA2.bin";
-        String fileName = "BAZA22.bin";
+        String fileName = "BAZA2.bin";
+        //String fileName = "BAZA22.bin";
         this.X = new ArrayList<>();
         this.Parameters = new ArrayList<>();
         this.Y= new ArrayList<>();
@@ -96,7 +107,8 @@ public class Main extends Application {
         LogarithmicAxis yAxis = new LogarithmicAxis("Range",0.1d,10000d);
 
 
-        LineChart<Double,Double> chart = new LineChart(xAxis,yAxis);
+        System.out.println(Parameters);
+        chart = new LineChart(xAxis,yAxis);
         chart.setAnimated(false);
 
         XYChart.Series<Double,Double> series = new XYChart.Series();
@@ -148,18 +160,21 @@ public class Main extends Application {
             update(button,button2);
         });
 
+
+        label = new Label("D/d: "+Parameters.get(3*s)+" Ri/Rm: "+Parameters.get(3*s+1) + " Rt/Rm: "+Parameters.get(3*s+2) );
+        
+        label.setTranslateX(150);
+
         StackPane spButton = new StackPane();
 
         spButton.setAlignment(Pos.TOP_LEFT);
-//        spButton.getChildren().add(button);
-//        spButton.getChildren().add(button2);
-        spButton.getChildren().addAll(button,button2);
+        spButton.getChildren().addAll(button,button2,label);
 
         VBox vbox = new VBox();
         VBox.setVgrow(spLineChart, Priority.ALWAYS);//Make line chart always grow vertically
         vbox.getChildren().addAll( spButton,spLineChart);
 
-        Scene scene  = new Scene(vbox,800,600);
+        scene  = new Scene(vbox,800,600);
         chart.getData().addAll(array.get(0),array.get(1),array.get(2),array.get(3),array.get(4),array.get(5),array.get(6),array.get(7),array.get(8),array.get(9),array.get(10),array.get(11),array.get(12),array.get(13),array.get(14),array.get(15),array.get(16),array.get(17),array.get(18),array.get(19),array.get(20),array.get(21),array.get(22),array.get(23),array.get(24),array.get(25),array.get(26),array.get(27),array.get(28),array.get(29),array.get(30),array.get(31),array.get(32),array.get(33),array.get(34),array.get(35),array.get(36),array.get(37));
 
         chart.setCreateSymbols(false);
@@ -169,17 +184,15 @@ public class Main extends Application {
 
         chart.applyCss();
         chart.layout();
+
+
+        setCtrlC();
+
         stage.setScene(scene);
         stage.show();
 
 
-            WritableImage image = chart.snapshot(new SnapshotParameters(),null);
-           // WritableImage image = new WritableImage(600,800);
 
-            chart.snapshot(new SnapshotParameters(),image);
-            ClipboardContent cc = new ClipboardContent();
-            cc.putImage(image);
-            Clipboard.getSystemClipboard().setContent(cc);
 
 
 //        Timeline timeline = new Timeline(new KeyFrame(Duration.millis(1000), e->run()));
@@ -188,46 +201,72 @@ public class Main extends Application {
     }
 
 
+
+    public void screenshot()
+    {
+        WritableImage image = chart.snapshot(new SnapshotParameters(),null);
+        // WritableImage image = new WritableImage(600,800);
+
+        chart.snapshot(new SnapshotParameters(),image);
+        ClipboardContent cc = new ClipboardContent();
+        cc.putImage(image);
+        Clipboard.getSystemClipboard().setContent(cc);
+    }
+
+
         public void run()
         {
 
         }
-        public void update(Button b1,Button b2)
-        {
+        public void update(Button b1,Button b2) {
             System.out.println(s);
 
-            if(s>=607)
-            {
+            if (s >= 607) {
                 b1.setDisable(true);
                 b2.setDisable(false);
             }
-            if(s<=0)
-            {
+            if (s <= 0) {
                 b2.setDisable(true);
                 b1.setDisable(false);
             }
 
-            if(s>0 && s <607)
-            {
+            if (s > 0 && s < 607) {
                 b2.setDisable(false);
                 b1.setDisable(false);
             }
 
 
-            for(int i =0; i<38;i++)
-            {
+            for (int i = 0; i < 38; i++) {
                 array.get(i).getData().clear();
-                for(int j = 0; j < 120;j++)
-                {
-                   // int temp = i*s*120+j;
+                for (int j = 0; j < 120; j++) {
+                    // int temp = i*s*120+j;
 
-                    int temp = s*(120*38)+(i*120)+j;
+                    int temp = s * (120 * 38) + (i * 120) + j;
 
-                    array.get(i).getData().add(new XYChart.Data(pow(10,X.get(j)),pow(10,Y.get(temp))));
+                    array.get(i).getData().add(new XYChart.Data(pow(10, X.get(j)), pow(10, Y.get(temp))));
 
                 }
             }
+
+            label.setText("D/d: "+Parameters.get(3*s)+" Ri/Rm: "+Parameters.get(3*s+1) + " Rt/Rm: "+Parameters.get(3*s+2));
         }
+
+
+    final KeyCombination keyCombinationShiftC = new KeyCodeCombination(KeyCode.C, KeyCombination.CONTROL_DOWN);
+
+   // public CommandLine command_line = new CommandLine();
+    public void setCtrlC() {
+
+        scene.setOnKeyPressed(new EventHandler<KeyEvent>() {
+
+            @Override
+            public void handle(KeyEvent event) {
+                if (keyCombinationShiftC.match(event)) {
+                    screenshot();
+                }
+            }
+        });
+    }
     public static void main(String[] args) {
         launch(args);
     }
